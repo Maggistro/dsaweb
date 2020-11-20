20fe68c250dae2ab52f3960841a94eb58c88cc1a
Maggistro <maggistro@googlemail.com>
2020-11-20 12:44:01 +0100

    Change app to tsx

---
+++ app/pages/_app.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppProps } from 'next/app'
+
+function App({Component, pageProps}: AppProps )
+{
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export default App;
